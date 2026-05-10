6f7ce332d3d79e8491619a416d536852e8d92c60
Kweeku <adubenedict10@gmail.com>
2026-05-10 11:13:35 +0000

    Core Data implementation

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,17 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Benedict Adu on 10/05/2026.
+//  Copyright © 2026 App Brewery. All rights reserved.
+//
+
+// This is for Realm
+
+//import Foundation
+//import RealmSwift
+//
+//class Item: Object {
+//    @objc dynamic var title : String = ""
+//    @objc dynamic var done : Bool = false
+//}
